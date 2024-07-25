8e8bb614f0e7a92ed9647abadced545eb6c0dcb8
calvinyueWillTry <calvin.yue@gmail.com>
2024-07-24 21:02:44 -0700

    can't get id in query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,11 @@
-const Review = require("../models/Review");//just models?
+const Review = require("../models/Review");//referring to the index in models
 
 const resolvers = {
     Query: {
-        review: async (parent, args) => {//put in parent and arguments just to have access to context
-            
-            const review = await Review.findOne( args );
+        review: async (parent, args, context) => {//put in parent and arguments just to have access to context
+            console.log(parent);//context appears to be the only item producing anything in the console log
+            const review = await Review.findOne( args );//why can't this take in an id? 
+            //can only take the very first entry ever created 
             return review;
           },
     },
@@ -18,43 +19,28 @@ const resolvers = {
  
 module.exports = resolvers;
 /**
- * Review: {
-        authors: async (parent, args, context) => {
-            if (parent.authors) { //fetches and finds the authors field for that Review object
-                const reviewData = await Review.findOne({ _id: parent.authors._id }).select("authors");
-                return reviewData.authors;
-            } else {
-                console.log("error in resolvers");
-            }
-        },//returns the description field of the Review object
-        description: async (parent) => {
-            return parent.description;
-        }
-    },
-    Mutation: { //creates a new Review object with the provided arguments, 
-        addReview: async (parent, args) => {
-            const newReview = new Review(args);
-            const result = await newReview.save();
-            return result;
-        } //saves it to the database, and returns the result
+ * 
+ * const review = await Review.findOne( { _id: context.authors._id } );
+ * "message": "Cannot read properties of undefined (reading '_id')
+ * const review = await Review.findOne( { context } );
+ * BSONError: Cannot convert circular structure to BSON
+ * const review = await Review.findOne( { parent, args } );
+ * {
+  "data": {
+    "review": null
+  }
+}
+const review = await Review.findOne( { args } );
+parent = {} (it's undefined), args = {} - nothing inside
+const review = await Review.findOne( args );
+{
+  "data": {
+    "review": {
+      "description": "try",
+      "authors": null,
+      "_id": "66a084318a98442ffb3d67f2",
+      "product": "iPhone 7"
     }
- */
-/** 
- * // Query: {
-    //     review: async (parent, args, context) => {
-    //         // Check if there is a user authenticated
-    //         if (!context.user) {
-    //             throw new Error('You are not authenticated.');
-    //         }
-
-    //         try {
-    //             // Fetch the user details based on the authenticated user's ID
-    //             const user = await User.findById(context.user.id);
-    //             return user;
-    //         } catch (error) {
-    //             console.error(error);
-    //             throw new Error('Failed to fetch user details.');
-    //         }
-    //     }
-    // },
+  }
+} //but this is the only one that appears
  */
--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -2,20 +2,16 @@ const typeDefs = `
 type Query {
   review: Review
 }
-
 type Review {
   _id: ID
   product: String!
   authors: String
   description: String!
 }
-
 type Mutation {
   addReview(product: String!, authors: String, description: String!): Review
 }
-
 `;
-
 module.exports = typeDefs;
 //not using these (for now)
 /**
